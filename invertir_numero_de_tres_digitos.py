5e6ae5d876916731e8c0e60d3abec558259066d1
DannyGR03 <DannyGR03@users.noreply.github.com>
2023-02-25 17:30:51 -0500

    se agrego diagrama de flujo

--- invertir_numero_de_tres_digitos.py
+++ invertir_numero_de_tres_digitos.py
@@ -8,17 +8,18 @@ x = int(input("digite el valor de x: "))
 y = int(input("digite el valor de y: "))
 z = int(input("digite el valor de z: "))
 
-c = x * 100
-d = y * 10
-u = z * 1
-t = c + d + u
+a = z * 100
+b = y * 10
+c = x * 1
+ni = a + b + c
+
 
 # output
 
 print("-----------------------")
 print("---------- resultados -----------")
 print("-----------------------")
+print("Multiplicación: " + str(a))
+print("Multiplicación: " + str(b))
 print("Multiplicación: " + str(c))
-print("Multiplicación: " + str(d))
-print("Multiplicación: " + str(u))
-print("Suma " + str(t))
+print("Suma " + str(ni))
